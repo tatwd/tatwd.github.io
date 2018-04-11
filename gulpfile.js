5612a5a76fc492bebecbb8b293d42006fe5ec2c3
tatwd <leoking9641@hotmail.com>
2018-04-11 09:08:07 +0800

    minify pages

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,19 @@ var gulp     = require('gulp');
 var htmlmin  = require('gulp-htmlmin');
 var htmlBeautify = require('gulp-html-beautify');
 
-gulp.task('beautify:html', () => {
+gulp.task('format:html', () => {
     return gulp.src('public/**/*.html')
         .pipe(htmlmin({
             collapseWhitespace: true, // 压缩 html
             minifyJS: true,           // 压缩页面 Js
             minifyCSS: true           // 压缩页面 css
         }))
-        .pipe(htmlBeautify({
-            indentSize: 2
-        }))
+        // .pipe(htmlBeautify({
+        //     indentSize: 2
+        // }))
         .pipe(gulp.dest('public/'));
 });
 
-gulp.task('default', ['beautify:html'], () => {
+gulp.task('default', ['format:html'], () => {
     console.log('return 0');
 });
