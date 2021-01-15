6832d3b7a2ead6baae773d3c8d3e04306e12e43a
tatwd <leoking9641@hotmail.com>
2021-01-16 00:48:08 +0800

    build: update hexo -> 5.3.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,39 @@
-var gulp = require('gulp');
-var htmlmin = require('gulp-htmlmin');
+const { series, src, dest } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
-function format(cb) {
-  gulp
-    .src('public/**/*.html')
+function minifyHTML(cb) {
+  src('public/**/*.html')
     .pipe(
       htmlmin({
         collapseWhitespace: true,
         minifyJS: true,
-        minifyCSS: true
+        minifyCSS: true,
       })
     )
-    .pipe(gulp.dest('public/'));
+    .pipe(dest('public/'));
   cb();
 }
 
-exports.fmt = format;
+function minifyCSS(cb) {
+  src('public/**/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie10' }))
+    .pipe(dest('public/'));
+  cb();
+}
+
+function minifyJS(cb) {
+  src('public/**/*.js')
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      })
+    )
+    .pipe(uglify())
+    .pipe(dest('public/'));
+  cb();
+}
+
+exports.default = series(minifyHTML, minifyCSS, minifyJS);
